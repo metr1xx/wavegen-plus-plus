fcafd16d4eac1fc36bfedd281c937e9ea09de45a
Leonard Lorenz <contact@metr1xx.de>
2018-10-04 13:43:05 +0200

    defined global variables with #define instead of const int. Created .gitignore to ignore build files. Created file writer function.

--- main.cpp
+++ main.cpp
@@ -1,35 +1,63 @@
 #define _USE_MATH_DEFINES
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <string>
 #include <cstdlib>
 #include <vector>
 #include <cmath>
 
-const int num_samples = 48000;
-const int sampling_rate = 48000;
-const int amplitude = 16000;
-const int bit_depth = 16;
+#define NUM_SAMPLES 48000
+#define SAMPLING_RATE 48000
+#define AMPLITUDE 16000
+#define BIT_DEPTH 16
+
 
 void print_sample(float &sample){ std::cout << sample << std::endl; }
 
 void write_wave(std::vector<float> &wave, const std::string filename){
+    // create wave buffer
+    std::stringstream buffer;
+    for(float sample : wave){
+        buffer << sample;
+    }
+
+    // write buffer to file
+    std::ofstream file;
+    file.open(filename);
+    file << buffer;
+    file.close();
 }
 
 void generate_sine(const int &frequency, const int &playtime, const std::string &filename){
-    int block_size = playtime * num_samples;
+    int block_size = playtime * NUM_SAMPLES;
     // this vector will hold the sample data
     std::vector<float> wave(block_size);
     // this loop calculates all the wave samples until 
     // the end of the defined playtime
-    for(int i = 0; i < num_samples * playtime; i++){
-        wave[i] = sin(2 * M_PI * frequency * i/sampling_rate);
+    for(int i = 0; i < NUM_SAMPLES * playtime; i++){
+        wave[i] = sin(2 * M_PI * frequency * i/SAMPLING_RATE);
         print_sample(wave[i]);
     }
     write_wave(wave, filename);
 }
 
-void generate_saw(const int &frequency, const int &playtime, const std::string &filename){}
+void generate_saw(const int &frequency, const int &playtime, const std::string &filename){
+    int block_size = playtime * NUM_SAMPLES;
+    // this vector will hold the sample data
+    std::vector<float> wave(block_size);
+    // this loop calculates all the wave samples until 
+    // the end of the defined playtime
+    float x = 0;
+    for(int i = 0; i < NUM_SAMPLES * playtime; i++){
+        wave[i] = x * (2 * M_PI * frequency * i/SAMPLING_RATE);
+        x = x + 0.1;
+        if (x > 1) x = -1;
+        print_sample(wave[i]);
+    }
+    write_wave(wave, filename);
+}
 void generate_triangle(const int &frequency, const int &playtime, const std::string &filename){}
 void generate_square(const int &frequency, const int &playtime, const std::string &filename){}
 
@@ -41,7 +69,7 @@ void gen_wave(const std::string &wave_type, const int &frequency, const int &pla
         generate_sine(frequency, playtime, filename);
     } else if (wave_type == "saw"){
         std::cout << "Generating saw wave" << std::endl;
-        //generate_saw(frequency, playtime, filename);
+        generate_saw(frequency, playtime, filename);
     } else if (wave_type == "triangle"){
         std::cout << "Generating triangle wave" << std::endl;
         //generate_triangle(frequency, playtime, filename);
@@ -53,7 +81,7 @@ void gen_wave(const std::string &wave_type, const int &frequency, const int &pla
 
 void print_help(){
     std::cout << "Usage:" << std::endl <<
-                 "wavegen <wavetype> <frequency> <playtime> <filename>";
+                 "wavegen <wavetype> <frequency> <playtime> <filename>" << std::endl;
 }
 
 int main(int argc, char** argv){
@@ -64,7 +92,7 @@ int main(int argc, char** argv){
     const int wave_frequency = atoi(argv[2]);
     const int wave_playtime = atoi(argv[3]);
     const std::string filename = argv[4];
-    if(argc != 4){
+    if(argc != 5){
         print_help();
         return 1;
     } else {
